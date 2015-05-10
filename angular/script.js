0d39f3fb8b2ab58580451cab35d732bed6ddb1b3
luis hugo rodriguez <luis4god@gmail.com>
2015-05-09 22:04:20 -0300

    Clase 1 angujar

--- angular/script.js
+++ angular/script.js
@@ -1,8 +1,17 @@
 var app = angular.module('educit',[]);
 
-app.controller('Facturacion',Facturacion);
+app
+.controller('Facturacion',Facturacion)
+.controller('Persona',Persona);
 
 function Facturacion($scope){
+  var total = 32;
+    $scope.nombre='Facturacion';
+    $scope.verDatos = function(){
+      return $scope.nombre + ' ' + total;
+    }
+}
+function Persona($scope){
   var edad = 32;
     $scope.nombre='Luis';
     $scope.verDatos = function(){
